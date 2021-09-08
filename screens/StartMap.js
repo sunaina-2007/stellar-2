b40875ddc5127d89fa8a4d7af641ef689fb8e3da
sunaina-2007 <80744244+sunaina-2007@users.noreply.github.com>
2021-09-08 12:11:59 +0530

    Add files via upload

---
+++ screens/StartMap.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class StartMapScreen extends Component {
+    render(){
+    return(
+    <View style = {{flex: 1,justifyContent: 'center',alignItems: 'center'}}>    
+    <Text>Start Map Screen</Text>
+    </View>
+    )    
+    }    
+    }
